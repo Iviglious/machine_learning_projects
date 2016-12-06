e1dca4b447de579ba113cd73d5919d49d291f90b
ec16030 <y.chang@se16.qmul.ac.uk>
2016-12-06 01:44:58 +0000

    1. Added main.Rmd notebook 2. Removed duplicated script.

--- FaceKPDetection/parser.R
+++ FaceKPDetection/parser.R
@@ -1,23 +1,15 @@
-library(doMC)
-registerDoMC()
 
-# Data input
+FKD_load <- function(TRAIN.FILE, TEST.FILE){
+	library(doMC)
+	registerDoMC()
 
-data.dir <- 'dataset/'
+	d.train    <- read.csv(TRAIN.FILE, stringsAsFactors=F)
+	d.test    <- read.csv(TEST.FILE, stringsAsFactors=F)
+	im.train   <- foreach(im = d.train$Image, .combine=rbind) %dopar% { as.integer(unlist(strsplit(im, " "))) }
+	im.test    <- foreach(im = d.test$Image, .combine=rbind) %dopar% { as.integer(unlist(strsplit(im, " "))) }
 
-train.file <- paste0(data.dir, 'training.csv')
-test.file  <- paste0(data.dir, 'test.csv')
-out.rdata <- paste0(data.dir, 'dataset.Rd')
-d.train    <- read.csv(train.file, stringsAsFactors=F)
-d.test     <- read.csv(test.file,  stringsAsFactors=F)
-im.train   <- foreach(im = d.train$Image, .combine=rbind) %dopar% {
-	as.integer(unlist(strsplit(im, " ")))
+	d.train$Image <- NULL
+	d.test$Image <- NULL
+
+	return(list("d.train" = d.train, "im.train" = im.train, "im.test" = im.test))
 }
-im.test    <- foreach(im = d.test$Image, .combine=rbind) %dopar% {
-	as.integer(unlist(strsplit(im, " ")))
-}
-
-d.train$Image <- NULL
-d.test$Image  <- NULL
-
-save(d.train, im.train, d.test, im.test, file= out.rdata)
